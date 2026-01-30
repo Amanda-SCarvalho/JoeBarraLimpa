9e91240bb8aff60cd932027bdae092030495d285
Amanda-SCarvalho <amanda.s.carvalho15@gmail.com>
2026-01-30 18:28:45 -0300

    pequenas correções

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
+
 
 export async function GET() {
   const users = await prisma.admin.findMany();
@@ -8,16 +8,4 @@ export async function GET() {
 }
 
 
-/* export async function GET() {
-const users = await prisma.admin.findMany({
-  select: {
-    id: true,
-    username: true,
-    role: true,
-    active: true,
-    createdAt: true,
-  } satisfies Prisma.AdminSelect,
-});
 
-  return NextResponse.json(users);
-} */
